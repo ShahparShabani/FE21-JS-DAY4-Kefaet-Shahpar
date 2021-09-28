41c12cb4880fb52f0018800c84e282bd6c1d7a99
shahparshabani <shahpar.shabani@g-experts.net>
2021-09-28 13:14:27 +0200

    add sh- basic exercise 2

--- Shahpar/js/basic.js
+++ Shahpar/js/basic.js
@@ -1,8 +1,9 @@
+// Ex 1
 function printInfo() {
   let fName = document.querySelector("#f-name").value;
   let lName = document.querySelector("#l-name").value;
   let age = document.querySelector("#age").value;
-  
+
   let userInfo = document.querySelector("#info");
 
   addDiv(fName, userInfo);
@@ -15,4 +16,39 @@ function addDiv(text, userInfo) {
 }
 
 document.getElementById("btn-submit").addEventListener("click", printInfo);
-console.log(document.getElementById("btn"));
+
+// Ex 2
+function printInfo2() {
+  let fName = document.querySelector("#f-name2").value;
+  let lName = document.querySelector("#l-name2").value;
+  let age = document.querySelector("#age2").value;
+  let jobTitle = document.querySelector("#job-title").value;
+
+  let userInfo = document.querySelector("#info2");
+
+  addDiv2(fName, userInfo, false);
+  addDiv2(lName, userInfo, false);
+  addDiv2(age, userInfo, false);
+  addDiv2(jobTitle, userInfo, true);
+}
+
+function addDiv2(text, userInfo, isJobTitle) {
+  if (isJobTitle && text == "IT") {
+    userInfo.innerHTML += `<div style="background-color: purple;"> ${text} </div>`;
+  } else if (isJobTitle && text == "hospitality") {
+    userInfo.innerHTML += `<div style="background-color: yellow;"> ${text} </div>`;
+  } else {
+    userInfo.innerHTML += `<div> ${text} </div>`;
+  }
+}
+
+function styleJobtitle(value) {
+  let element = document.querySelector("#select-option");
+  if (value == "IT") {
+    element.style.backgroundColor = "purple";
+  } else {
+    element.style.backgroundColor = "yellow";
+  }
+}
+
+document.getElementById("btn-submit2").addEventListener("click", printInfo2);
